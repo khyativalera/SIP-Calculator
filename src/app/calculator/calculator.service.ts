8007f5087dfa1a9b5ee7468a563691b6a8bdc676
Khyati Valera <khyati.valera18@gmail.com>
2020-12-18 21:47:20 +0530

    first commit

---
+++ src/app/calculator/calculator.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculatorService {
+
+  calculateSIP(value: any): number {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor() { }
+}
